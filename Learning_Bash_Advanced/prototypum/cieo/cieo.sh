f4d56609991dcf83cffc42d5ba004bcb9afe54ef
Manuel Veliz <mveliz@mx1.ibm.com>
2017-01-25 11:02:55 -0600

    Modificación para el archivo cieo.sh para jalar con el nuevo plugin de vim para documentar correctamente. Actualizacion del archivo indicium.txt con un job para lnvo.

---
+++ Learning_Bash_Advanced/prototypum/cieo/cieo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: cieo.sh
+# 
+#         USAGE: ./cieo.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), sd
+#  ORGANIZATION: 
+#       CREATED: 24/01/17 16:33:51
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
