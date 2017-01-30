b98d346ae8c53819052a2bcece61b12a564df758
Manuel Veliz <mveliz@mx1.ibm.com>
2017-01-30 14:35:51 -0600

    Se termino el archivo mv to file.

--- Learning_Bash_Advanced/mi_cominenzo/move_that_file.sh
+++ Learning_Bash_Advanced/mi_cominenzo/move_that_file.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 #let's se if there's a file in ~/Desktop
-declare FILE
+declare -a FILES
 declare FECHA
+declare DIA
+declare FILE2
+declare DESKTOP
+declare HERE
 declare -a LISTA_DE_ARCHIVOS
-LISTA_DE_ARCHIVOS=$(ls /home/$USER/Desktop/) #If "ls -l" shows any file this file will match with next format "NotesX_YYYY_M_D_HH_M_SS_Response_to_INCIDENT_.pdf"
+#Set Working Directory
+HERE="$(pwd)"
+#Set Desktop PATH
+DESKTOP=/home/$USER/Desktop/
+#We set the current day
+DIA="$(date +*%Y_*%d*%H*)"
+#Put entire list from ~/Desktop/
+LISTA_DE_ARCHIVOS="$(ls /home/$USER/Desktop/)" #If "ls -l" shows any file this file will match with next format "NotesX_YYYY_M_D_HH_M_SS_Response_to_INCIDENT_.pdf"
+FILES="$(find $DESKTOP -type f -name $DIA)"
+#printf "%s\n" "${FILES[@]}"
+#Show the list content
 if [[ -n $LISTA_DE_ARCHIVOS ]];  then
-    echo "File does exists..."
-    FECHA=$(date +"%Y_%m_%d_%H_%M")
-    echo ${LISTA_DE_ARCHIVOS[*]}
-    echo "NotesX"
+    #echo "File does exists..."
+    #echo
+    #printf "%s\n" "${LISTA_DE_ARCHIVOS[@]}"
+    mv $FILES $HERE
+#   printf "%s\n" "${LISTA_DE_ARCHIVOS[@]}"
+    #echo `expr "${LISTA_DE_ARCHIVOS[@]}"`
+  else
+    echo "There is no files in $DESKTOP ..."
 fi
+
 #EOF
