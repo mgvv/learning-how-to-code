9872589a353b11ac6e402f057e4613f9e6ce4ef6
Manuel Veliz <mveliz@mx1.ibm.com>
2017-02-24 15:54:19 -0600

    Modificacion del script para mejorarlo 	modified:   Learning_Bash_Advanced/Pluralsight/Shell_Scripting_With_Bash.txt 	modified:   Learning_Bash_Advanced/mi_cominenzo/move_that_file.sh

--- Learning_Bash_Advanced/mi_cominenzo/move_that_file.sh
+++ Learning_Bash_Advanced/mi_cominenzo/move_that_file.sh
@@ -1,36 +1,22 @@
-#!/bin/bash 
+#!/bin/bash -
 #let's se if there's a file in ~/Desktop
-declare fecha
-declare dia
-declare mes
-declare year
-declare file2
-declare desktop
-declare here
-#File list certain criteria $dia
-declare -a files
-#File list at ~/Desktop/
-declare -a lista_de_archivos
 #Set Working Directory
 here="$(pwd)"
 #Set Desktop PATH
-desktop=/home/$USER/Desktop/
-#Workgin with date
-#We set the current day
-dia="$(date +%d)"
-dia="$(expr substr $dia 2 1)"
-mes="$(date +%m)"
-mes="$(expr substr $mes 2 1)"
+desktop="$HOME/Desktop/"
+#Working with date command output
+mes="$(date +%m | cut -c2)"
 year="$(date +%Y)"
-fecha="*${year}_${mes}_${dia}*"
+#Setting the find command criteria
+fecha="*${year}_${mes}_*"
 #Put entire list from ~/Desktop/
-lista_de_archivos="$(ls /home/$USER/Desktop/)" #If "ls -l" shows any file this file will match with next format "NotesX_YYYY_M_D_HH_M_SS_Response_to_INCIDENT_.pdf"
 files="$(find $desktop -type f -name $fecha)"
-#printf "%s\n" "${files[@]}"
-#Show the list content
-if [[ -n $lista_de_archivos ]] && [[ -n $files ]] ;  then
+#Evaluate the list content
+if [[ -n $files ]] ;  then
     mv $files $here
+    exit 0
   else
     echo "There is no files in $desktop ..."
+    exit 1
 fi
 #EOF
